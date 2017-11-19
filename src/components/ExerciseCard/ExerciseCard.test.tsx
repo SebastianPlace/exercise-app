bbc612fbf68764fbb9425a463f4a7d104abb979e
Sebastian Place <sebplace94@gmail.com>
2017-11-19 18:30:39 +0000

    Add test for tags

--- src/components/ExerciseCard/ExerciseCard.test.tsx
+++ src/components/ExerciseCard/ExerciseCard.test.tsx
@@ -6,7 +6,7 @@ import Exercise from '../../interfaces/exercise';
 const fakeExercise: Exercise = {
   id: '1',
   name: 'Test',
-  muscle_groups: ['chest']
+  muscle_groups: ['chest', 'core']
 };
 
 it('renders without crashing', () => {
@@ -18,4 +18,13 @@ it('renders the exercise name', () => {
   const div = document.createElement('div');
   ReactDOM.render(<ExerciseCard exercise={fakeExercise} key={fakeExercise.id} />, div);
   expect(div.getElementsByTagName('h2')[0].innerHTML).toBe('Test');
+});
+
+it('has a list of muscle groups', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ExerciseCard exercise={fakeExercise} key={fakeExercise.id} />, div);
+  const tags = div.getElementsByClassName('Tag');
+  expect(tags.length).toBe(2);
+  expect(tags[0].innerHTML).toBe('chest');
+  expect(tags[1].innerHTML).toBe('core');
 });
